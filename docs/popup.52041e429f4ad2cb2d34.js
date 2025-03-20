7968efc75c65cf920070cd0465beebb990e2974e
smnvkh <s.masha@me.com>
2025-03-20 13:37:53 +0500

    build

--- docs/popup.52041e429f4ad2cb2d34.js
+++ docs/popup.52041e429f4ad2cb2d34.js
@@ -4,10 +4,14 @@ var popups = document.querySelectorAll('.W_popupOverlay');
 var cardButtons = document.querySelectorAll('.O_cardInfo');
 var closeButtons = document.querySelectorAll('.Q_closeButton');
 var popupContents = document.querySelectorAll('.O_moreCardInfo');
-var body = document.querySelector('.cards');
+var body = document.querySelector('.popup');
+var zIndexChange = document.querySelectorAll('.W_cards, .W_articles, .W_tests .O_chapter');
 function openPopup(index) {
   popups[index].classList.add('activePopup');
   body.classList.add('scrollLock');
+  zIndexChange.forEach(function (element) {
+    element.style.zIndex = 'auto';
+  });
 }
 cardButtons.forEach(function (button, index) {
   button.addEventListener('click', function () {
@@ -19,6 +23,9 @@ function closeAllPopups() {
     popup.classList.remove('activePopup');
   });
   body.classList.remove('scrollLock');
+  zIndexChange.forEach(function (element) {
+    element.style.zIndex = '';
+  });
 }
 closeButtons.forEach(function (button) {
   button.addEventListener('click', closeAllPopups);
