7dae24f8b36ae5c95d08c65380777a1dee610718
smnvkh <s.masha@me.com>
2025-03-26 19:52:45 +0500

    ham menu

--- src/index.js
+++ src/index.js
@@ -15,3 +15,17 @@ if (pageName === 'cards.html') {
 if (pageName === 'tests.html') {
   document.querySelector('#tests').classList.add('activePage')
 }
+
+const hamMenu = document.querySelector('.M_Navigation.hamMenu')
+const closeMenu = document.querySelector('.M_Navigation.closeMenu')
+const offScreenMenu = document.querySelector('.M_Navigation.overlay')
+
+hamMenu.addEventListener('click', () => {
+  hamMenu.classList.add('active')
+  offScreenMenu.classList.add('active')
+})
+
+closeMenu.addEventListener('click', () => {
+  hamMenu.classList.remove('active')
+  offScreenMenu.classList.remove('active')
+})
